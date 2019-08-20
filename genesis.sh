bc0dff6b8a4eff30b5b6f438409f1ee9f4feb1d2
Federico Gimenez <fgimenez@coit.es>
2019-08-20 14:11:53 +0200

    specify python version and pass additional args

--- genesis.sh
+++ genesis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yarn genesis --endpoint $HTTP_ENDPOINT --atBlock $AT_BLOCK
-python post_processing.py
+yarn genesis --endpoint $HTTP_ENDPOINT --atBlock $AT_BLOCK "$@"
+python3 post_processing.py
 rm kusama.tmp.json
