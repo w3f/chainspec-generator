fca73fc1aee495130f59492255e8bb18dd60a27b
Federico Gimenez <fgimenez@coit.es>
2019-08-20 14:20:09 +0200

    no need for additional args

--- genesis.sh
+++ genesis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yarn genesis --endpoint $HTTP_ENDPOINT --atBlock $AT_BLOCK "$@"
+yarn genesis --endpoint $HTTP_ENDPOINT --atBlock $AT_BLOCK
 python3 post_processing.py
 rm kusama.tmp.json
