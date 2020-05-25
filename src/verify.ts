05a322ebb4ca2e8a9d18df3bf6de0aa3d878f335
Logan Saether <X@logansaether.com>
2020-05-25 21:52:49 +0200

    Update SAFT

--- src/verify.ts
+++ src/verify.ts
@@ -95,6 +95,42 @@ const verify = async (cmd: any) => {
 
   const [holders, claimers] = getClaimers(tokenHolders);
 
+  /// Check the statements.
+  let statementsArray: string[] = [];
+  fs
+  .readFileSync("SAFT_accounts.csv", { encoding: "utf-8" })
+  .split("\n")
+  .forEach((line: any) => {
+    const [ethAddr, dotAddr] = line.split(",");
+    if (dotAddr) statementsArray.push(dotAddr);
+    if (ethAddr.startsWith("0x")) statementsArray.push(ethAddr);
+  })
+  
+  for (const addr of statementsArray) {
+    if (addr.startsWith("0x")) {
+      const statementKind = await api.query.claims.signing(addr);
+      if (!statementKind.toString()) {
+        const claim = await api.query.claims.claims(addr);
+        if (claim.toString()) {
+          throw `Found claim for ${addr} but one shouldn't exist.`
+        }
+      } else {
+        if (statementKind.toString() !== "Alternative") {
+          throw `${addr} should be SAFT but is not.`
+        }
+      }
+    }
+    if (addr.startsWith("1")) {
+      const ethAddr = await api.query.claims.preclaims(addr);
+      if (ethAddr.toString()) { 
+        const statementKind = await api.query.claims.signing(ethAddr.toString());
+        if (statementKind.toString() !== "Alternative") {
+          throw `${addr} should be SAFT but is not. Statement kind: ${statementKind.toString()}`
+        }
+      }
+    }
+  }
+
   const numOfSuccessAddrs = await validateBalanceAndVesting(w3, api, holders);
 
   // Todo: Not check yet
@@ -104,12 +140,29 @@ const verify = async (cmd: any) => {
     const encoded = Keyring.encodeAddress(Util.hexToU8a(pubkey), 0);
 
     const preclaim = await api.query.claims.preclaims(encoded);
+    
+    if (statementsArray.indexOf(encoded) === -1) {
+      // check it's a regular statement.
+      const statementKind = await api.query.claims.signing(preclaim.toString());
+      if (statementKind.toString() !== "Default") {
+        // The eth address should be in the statements array then.
+        let found = false;
+        for (const addr of statementsArray) {
+          if (addr.toLowerCase() === preclaim.toString().toLowerCase()) found = true;
+          else if (addr == preclaim.toString()) found = true;
+        }
+        if (!found) {
+          throw `${encoded} should have regular statement but has something else. Has: ${statementKind.toString()} | Preclaim: ${preclaim.toString()}`;
+        }
+      } 
+    } 
+    
     if (
       preclaim.toString().toLowerCase() !== claimer.ethAddress.toLowerCase()
     ) {
       throw `Preclaim mismatch: Expected ${
         claimer.ethAddress
-      } but got ${preclaim.toString()}`;
+      } but got ${preclaim.toString()} (Polkadot Address: ${encoded})`;
     }
 
     const claim = await api.query.claims.claims(claimer.ethAddress);
@@ -148,35 +201,6 @@ const verify = async (cmd: any) => {
 
     counter++;
     console.log(`OK: ${encoded}`);
-  }
-
-  /// Check the statements.
-  const statementsArray = fs
-  .readFileSync("SAFT_accounts.csv", { encoding: "utf-8" })
-  .split("\n");
-
-  for (const line of statementsArray) {
-    const [ethAddr, dotAddr] = line.split(",");
-    if (dotAddr && ethAddr.startsWith("0x")) {
-      const checkAddr = await api.query.claims.preclaims(dotAddr);
-      const statementKind = await api.query.claims.signing(checkAddr.toString());
-      if (statementKind.toString() !== "Alternative") {
-        throw `${dotAddr} with ${ethAddr} and ${checkAddr.toString()} should be SAFT but is not.`
-      }
-    } 
-    if (ethAddr.startsWith("0x")) {
-      const statementKind = await api.query.claims.signing(ethAddr);
-      if (!statementKind.toString()) {
-        const claim = await api.query.claims.claims(ethAddr);
-        if (claim.toString()) {
-          throw `Found claim for ${ethAddr} but one shouldn't exist.`
-        }
-      } else {
-        if (statementKind.toString() !== "Alternative") {
-          throw `${ethAddr} should be SAFT but is not.`
-        }
-      }
-    }
   }
 
   console.log("ALL ALTERNATIVE STATEMENTS CHECK OUT")
